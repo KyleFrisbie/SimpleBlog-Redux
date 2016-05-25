a6b65dc51ad22d77bb80d07262dd1d968464878d
Kyle L Frisbie <kyle.l.frisbie@gmail.com>
2016-05-24 20:19:00 -0600

    added new post component with link on index page

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,17 +1,25 @@
 import React, {Component} from 'react';
-import { connect } from 'react-redux';
-import { fetchPosts } from '../actions/index';
+import {connect} from 'react-redux';
+import {fetchPosts} from '../actions/index';
+import {Link} from 'react-router';
 
 class PostsIndex extends Component {
-  componentWillMount() {
-    this.props.fetchPosts();
-  }
+	componentWillMount() {
+		this.props.fetchPosts();
+	}
 
-  render() {
-    return (
-      <div>List of posts</div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<div className="text-xs-right">
+					<Link to="/posts/new" className="btn btn-primary">
+						Add a Post
+					</Link>
+				</div>
+				List of posts
+			</div>
+		);
+	}
 }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+export default connect(null, {fetchPosts})(PostsIndex);
